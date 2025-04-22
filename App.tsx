b436335c2b7567ba0ba38bcf53a3fd377c69034b
Pavel <112262251+KIYOKATA-1@users.noreply.github.com>
2025-04-22 14:17:32 +0500

    fixing status bar style

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ export default function App() {
 
   return (
     <>
-      <StatusBar barStyle="dark-content" backgroundColor="#fff" />
+      <StatusBar barStyle="light-content" backgroundColor="#000" />
       {authenticated ? (
         <HomeScreen />
       ) : (
