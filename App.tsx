7a1d3fbc031f88f3b5f05b42ebd123e3b8c38bc5
Pavel <112262251+KIYOKATA-1@users.noreply.github.com>
2025-04-22 14:16:42 +0500

    created a state to check for biometrics

--- App.tsx
+++ App.tsx
@@ -4,12 +4,16 @@ import HomeScreen from "./components/HomeScreen";
 import { StatusBar } from "react-native";
 
 export default function App() {
+  const [authenticated, setAuthenticated] = useState<boolean>(false);
+
   return (
     <>
-      <StatusBar barStyle="light-content" backgroundColor="#fff" />
-      <LoginScreen/>
-      {/* <HomeScreen/> */}
-
+      <StatusBar barStyle="dark-content" backgroundColor="#fff" />
+      {authenticated ? (
+        <HomeScreen />
+      ) : (
+        <LoginScreen onAuthSuccess={() => setAuthenticated(true)} />
+      )}
     </>
   );
 }
